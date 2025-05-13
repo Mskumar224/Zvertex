adb70c151730c249d9648e50e08e83e7d2b71cba
sateesh <sathishjava224@gmail.com>
2025-05-13 13:52:30 -0500

    changed all UI and updated recurring jobs

--- server/models/Job.js
+++ server/models/Job.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
-  jobId: String,
   title: String,
   company: String,
-  link: String,
-  applied: { type: Boolean, default: false },
-  requiresDocs: Boolean,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  profile: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
-  contactEmail: String,
-  contactPhone: String,
-  createdAt: { type: Date, default: Date.now },
+  location: String,
+  postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('Job', jobSchema);
